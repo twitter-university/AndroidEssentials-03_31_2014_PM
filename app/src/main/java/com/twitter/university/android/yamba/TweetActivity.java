482d3032c652ba6e9d766eb56259681f59ea721d
G. Blake Meike <bmeike@twitter.com>
2014-03-31 19:20:57 -0700

    Basic Tweet page

---
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -0,0 +1,15 @@
+package com.twitter.university.android.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class TweetActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+    }
+}
