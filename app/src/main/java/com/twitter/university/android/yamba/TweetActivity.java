07a9033d80b8e145d721f55a8b2325d4f364f32a
G. Blake Meike <bmeike@twitter.com>
2014-04-02 11:25:33 -0700

    Styled Portrait and Landscape layouts

--- app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -2,14 +2,46 @@ package com.twitter.university.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 
 public class TweetActivity extends Activity {
+    private static final String TAG = "TWEET";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "in onCreate: " + this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "in onStart: " + this);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "in onResume: " + this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "in onPause: " + this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "in onStop: " + this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "in onDestroy: " + this);
+        super.onDestroy();
     }
 }
